fc065b4493de82f06f116cb003a2341a42d99050
jonben <eliejonathan.eb@gmail.com>
2024-06-06 10:58:51 +0300

    commit with validator added

--- user-management-service/src/routes/userRoutes.js
+++ user-management-service/src/routes/userRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const { loginUser, registerUser, getUsers, updateUser, deleteUser } = require('../controllers/userController');
 const auth = require('../middleware/authMiddleware');
 const admin = require('../middleware/adminAuthMiddleware');
+const { validateRegisterUser, validateLoginUser } = require('../utils/validator');
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+
+router.post('/register', validateRegisterUser, registerUser);
+router.post('/login', validateLoginUser, loginUser);
 router.get('/', auth, admin, getUsers);//only admin can getUsers
 router.put('/:id', auth, admin, updateUser);//only admin can updateUser
 router.delete('/:id', auth, admin, deleteUser);//only admin can deleteUser
