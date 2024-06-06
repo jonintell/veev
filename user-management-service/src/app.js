a0821c714b09597fbdb4e2e3a7fadf83c35d5269
jonben <eliejonathan.eb@gmail.com>
2024-06-06 09:42:34 +0300

    User model commit

--- user-management-service/src/app.js
+++ user-management-service/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-// const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
@@ -8,6 +7,5 @@ connectDB();
 
 app.use(express.json());
 
-// app.use('/api/users', userRoutes);
 
 module.exports = app;
